4a4826e605b12e2d1977b03d705c46499a538015
Hannah Kellett <hannahkellett2@gmail.com>
2024-04-17 14:17:20 +0100

    Added project files

---
+++ bug.cpp
@@ -0,0 +1,5 @@
+//
+// Created by D00260463 on 17/04/2024.
+//
+
+#include "bug.h"
---
+++ bug.h
@@ -0,0 +1,26 @@
+//
+// Created by D00260463 on 17/04/2024.
+//
+#include <iostream>
+#include <list>
+#ifndef ABUGSLIFE_BUG_H
+#define ABUGSLIFE_BUG_H
+
+using namespace std;
+class bug {
+
+    int id;
+    pair<int, int> position;
+    int direction; //TODO: replace with enum
+    int size;
+    bool alive;
+    list<pair<int,int>> path;
+
+    virtual void move()=0;
+
+    bool isWayBlocked();
+
+};
+
+
+#endif //ABUGSLIFE_BUG_H
